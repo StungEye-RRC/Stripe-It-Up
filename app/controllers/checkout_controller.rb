1ff7876450c5b4b44cb31899f8b4d5ffa6437b93
Kyle Geske <stungeye@gmail.com>
2019-11-20 16:03:16 -0600

    Implemented a buy now featuring using Stripe.

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class CheckoutController < ApplicationController
+  def create
+    product = Product.find(params[:id])
+
+    if product.nil?
+      redirect_to root_path
+      return
+    end
+
+    # Setting up a Stripe Session for payment.
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [{
+        name: product.name,
+        description: product.description,
+        amount: product.price_cents,
+        currency: 'cad',
+        quantity: 1
+      }],
+      success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: checkout_cancel_url
+    )
+
+    respond_to do |format|
+      format.js # render create.js.erb
+    end
+  end
+
+  def success
+    @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+  end
+
+  def cancel; end
+end
