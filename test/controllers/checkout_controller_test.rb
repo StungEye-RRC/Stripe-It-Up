6035c8337cc9e7b23ddd42068577be416b1cb014
Kyle Geske <stungeye@gmail.com>
2019-11-19 22:01:15 -0600

    Removed the checkout controller.

--- test/controllers/checkout_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CheckoutControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get checkout_new_url
-    assert_response :success
-  end
-
-end
