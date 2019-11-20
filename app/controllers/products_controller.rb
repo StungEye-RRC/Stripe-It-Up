3a3aed55a604de2e68ae43c31762e7bc9c723a1d
Kyle Geske <stungeye@gmail.com>
2019-11-20 15:06:17 -0600

    Implemented the session based shopping cart.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,42 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
+  before_action :initialize_session
+  before_action :increment_visit_count, only: %i[index about]
+  before_action :load_cart
+
   def index
     @products = Product.all
+  end
+
+  def about; end
+
+  def add_to_cart
+    id = params[:id].to_i
+
+    session[:cart] << id unless session[:cart].include?(id)
+    redirect_to root_path
+  end
+
+  def remove_from_cart
+    id = params[:id].to_i
+    session[:cart].delete(id)
+    redirect_to root_path
+  end
+
+  private
+
+  def initialize_session
+    session[:visit_count] ||= 0 # Initialize the visit count on first visit.
+    session[:cart] ||= []
+  end
+
+  def load_cart
+    @cart = Product.find(session[:cart])
+  end
+
+  def increment_visit_count
+    session[:visit_count] += 1  # Increment the count with each visit.
+    @visit_count = session[:visit_count]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'about', to: 'products#about'
+  post 'products/add_to_cart/:id', to: 'products#add_to_cart', as: 'add_to_cart'
+  delete 'products/remove_from_cart/:id', to: 'products#remove_from_cart', as: 'remove_from_cart'
+
   root to: 'products#index'
 end
